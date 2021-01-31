e404d8210f3cdbd3e63f8d1bb81c579942eb7aba
jhake <73086064+jhake-papa@users.noreply.github.com>
2021-02-01 03:31:18 +0800

    refactor to SpotifyTracks

--- src/pages/SpotifyStats/index.tsx
+++ src/pages/SpotifyStats/index.tsx
@@ -1,3 +1,4 @@
+import SpotifyTracks from "components/SpotifyTracks";
 import { useAxiosGet } from "hooks/useAxiosGet";
 import { useUrlQuery } from "hooks/useUrlQuery";
 
@@ -11,40 +12,16 @@ const SpotifyStats = () => {
     (offset ? `offset=${offset}&` : "") +
     (time_range ? `time_range=${time_range}&` : "");
 
-  console.log(url);
-
   const { data, loading, error } = useAxiosGet<any[]>(url);
-  const { data: lfData } = useAxiosGet(
-    process.env.REACT_APP_BACKEND_API_URL + "lf_top_tracks"
-  );
-
-  console.log(lfData);
-  console.log(data);
-  console.log(error);
 
   return (
     <>
       <h1>Spotify Stats</h1>
-      {loading
-        ? "loading"
-        : data?.map((d) => {
-            //return <p>{d.name}</p>;
-
-            return (
-              <>
-                <iframe
-                  src={`https://open.spotify.com/embed/track/${d.id}`}
-                  width={300}
-                  height={380}
-                  frameBorder={0}
-                  allowTransparency={true}
-                  allow="encrypted-media"
-                  style={{ borderRadius: "10px" }}
-                ></iframe>
-              </>
-            );
-          })}
-
+      {loading ? (
+        "loading"
+      ) : (
+        <SpotifyTracks spotifyTrackIds={data?.map((d) => d.id) ?? []} />
+      )}
       {error && "errors occured"}
     </>
   );
